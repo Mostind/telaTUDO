6fd8b0a3f03a03e4ed2b63c70e2bcfae4dc3df3d
mo0n-emi <emilly.almeida14@aluno.ce.gov.br>
2023-05-05 09:01:15 -0300

    atualizacao2

---
+++ categoria.php
@@ -0,0 +1,81 @@
+<?php 
+session_start();
+    //print_r($_SESSION);
+    if ((!isset($_SESSION['email']) == true) && (!isset($_SESSION['senha']) == true) ) 
+    {
+        unset($_SESSION['email']);
+        unset($_SESSION['senha']);
+        header("Location:login.php");
+    }
+    include_once('config.php');
+
+    $logado = $_SESSION['email'];
+    $categoriasQuery = 'SELECT * FROM categoria';
+    $categoria = $conexao->query($categoriasQuery);
+
+    if ($_SESSION['nivel']) 
+    {
+        $voltar="sistemagerente.php";    
+    }
+    else
+    {
+        $voltar="sistemagarcom.php";
+    }
+
+?>
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Produtos</title>
+    <link rel="stylesheet" href="./css/tabelaG.css">
+</head>
+
+<body>
+    <div class="tituloBot">        
+        <button class="botTit" onclick="javascript:history.go(-1)">Voltar</button>
+        <h3>PRODUTOS</h3>
+        <a href="cadastrocategoria.php" class="botTit">Nova Categoria</a>
+    </div>
+
+    <table>
+        <div class="grupo">
+                <div class="sub-grupo">idCategoria</div>
+                <div class="sub-grupo">nome</div>
+
+            </div>
+
+                <?php 
+                    while ($row = mysqli_fetch_array($categoria)) {
+                        echo '<div class="grupo2">';
+                        echo '<div class="sub-grupo2">'.$row['idCategoria']."</div>";
+                        echo '<div class="sub-grupo2">'.$row['nomeCategoria']."</div>";
+                        echo '<a href="categoriaedit.php?idCategoria='.$row['idCategoria'].'" class="sub-grupo2">Editar</a>
+                        ';
+                        echo '<div class="sub-grupo2"> <form method="post" action="categoria.php">
+                        <input type="hidden" name="idCategoria" value="'.$row['idCategoria'].'">
+                        <input type="submit" value="Deletar" name="excluir" onClick="return confirm(\'Você tem certeza?\');">
+                        </form>';
+                        echo "</div>";
+                        echo '</div>';
+                    }
+                ?>
+
+    </table>
+    <br>
+    
+</body>
+</html>
+<?php 
+    if (isset($_POST['excluir'])) {
+        $id = $_POST['idCategoria'];
+
+        $deleteCategoriaQuery = 'DELETE FROM categoria WHERE idCategoria ='.$id;
+        $conexao->query($deleteCategoriaQuery);
+        
+        
+        header('Location:categoria.php');
+    }
+?>
---
+++ produtos.php
@@ -0,0 +1,89 @@
+<?php 
+session_start();
+    //print_r($_SESSION);
+    if ((!isset($_SESSION['email']) == true) && (!isset($_SESSION['senha']) == true) ) 
+    {
+        unset($_SESSION['email']);
+        unset($_SESSION['senha']);
+        header("Location:login.php");
+    }
+    include_once('config.php');
+
+    $logado = $_SESSION['email'];
+    $produtosQuery = 'SELECT idProduto,nome,valor,c.nomeCategoria,quantEstoque FROM produto
+    JOIN categoria AS c
+    USING(idCategoria)';
+    $produtos = $conexao->query($produtosQuery);
+
+
+    if ($_SESSION['nivel']) 
+    {
+        $voltar="sistemagerente.php";    
+    }
+    else
+    {
+        $voltar="sistemagarcom.php";
+    }
+
+?>
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./css/tabela.css">
+    <title>Produtos</title>
+</head>
+
+<body>
+    
+    <div class="tituloBot">
+        <button class="botTit" onclick="javascript:history.go(-1)">Voltar</button>
+        <h3>PRODUTOS</h3>
+        <a href="cadastroproduto.php" class="botTit">Novo Produto</a>
+    </div>
+    
+    <table>
+        <div class="grupo">
+            <div class="sub-grupo">idProd.</div>
+            <div class="sub-grupo">Nome</div>
+            <div class="sub-grupo">Valor</div>
+            <div class="sub-grupo">Categoria</div>
+            <div class="sub-grupo">Quant. em Estoque</div> 
+            </div>
+
+                <?php 
+                    while ($row = mysqli_fetch_array($produtos)) {
+                        echo '<div class="grupo2">';
+                        echo '<div class="sub-grupo2">'.$row['idProduto']."</div>";
+                        echo '<div class="sub-grupo2">'.$row['nome']."</div>";
+                        echo '<div class="sub-grupo2">R$'.str_replace(".",",",$row['valor'])."</div>";
+                        echo '<div class="sub-grupo2">'.$row['nomeCategoria']."</div>";
+                        echo '<div class="sub-grupo2">'.$row['quantEstoque']."</div>";
+                        echo '
+                        <a class="sub-grupo2"href="produtosedit.php?idProduto=' .$row['idProduto'].'">Editar</a>';
+                        
+                        echo '<div class="sub-grupo2"> <form method="post" action="produtos.php">
+                        <input type="hidden" name="idProduto" value="'.$row['idProduto'].'">
+                        <input type="submit" value="Deletar" name="excluir" onClick="return confirm(\'Você tem certeza?\');">
+                        </form>';
+                        echo "</div>";
+                        echo '</div>';
+                    }
+                ?>
+    </table>
+
+</body>
+</html>
+<?php 
+    if (isset($_POST['excluir'])) {
+        $id = $_POST['idProduto'];
+
+        $deleteProdutoQuery = 'DELETE FROM produto WHERE idProduto ='.$id;
+        $conexao->query($deleteProdutoQuery);
+        
+        
+        header('Location:produtos.php');
+    }
+?>
